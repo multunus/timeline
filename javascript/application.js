9681f6fc1dadc144fc383a5ad763ffc98c5823ab
Abhilash M A <abhidsm@gmail.com>
2012-10-09 17:36:42 +0530

    Moved manoj's timeline to template

--- javascript/application.js
+++ javascript/application.js
@@ -75,11 +75,189 @@ setUpPopOverCloseOnClick = function() {
 	});
 };
 
-var Turks = [{name: 'manoj', display_name: 'Manoj'}, {name: 'tamil', display_name: 'Tamil'}, {name: 'akshay', display_name: 'Akshay'}, {name: 'sreenath', display_name: 'Sreenath'}, {name: 'anup', display_name: 'Anup'}, {name: 'ernest', display_name: 'Ernest'}, {name: 'abhilash', display_name: 'Abhilash'}, {name: 'kp', display_name: 'KP'}, {name: 'you', display_name: 'You?'}];
+var Timelines = [
+    {
+        name: 'manoj', 
+        display_name: 'Manoj',
+        roles: '<a rel="tooltip" title="AKA Multunus Senior :)">Multunus Se&ntilde;or</a>, Programmer, Designer, Speaker',
+        major: [
+            {
+                year: '2012',
+                minor: [
+                    {
+                        month: 'September 2012',
+                        events: [
+                            'Chosen as speaker for the upcoming JSFoo Bangalore conference. <a target="_blank" href="http://youtu.be/EAEfEByoJeA">Watch Preview Video</a>',
+                            'Lead programmer on Narrable.com - a different kind of story telling site'
+                        ]
+                    },
+                    {
+                        month: 'June 2012',
+                        events: [
+                            'Active involvement in the Bangalore Ruby User Group through Multunus',
+                            'Inducted into the Multunus Señors group (also called the "Seniors" group :)'
+                        ]
+                    },
+                    {
+                        month: 'April 2012',
+                        events: [
+                            'Promoted as Senior UI Programmer'
+                        ]
+                    },
+                    {
+                        month: 'March 2012',
+                        events: [
+                            'Introduced to Internet marketing and did some work on FB and hiring related marketing',
+                            'Finished 3 years at Multunus'
+                        ]
+                    },
+                    {
+                        month: 'Feb 2012',
+                        events: [
+                            'Was the main designer for a highly interactive web app - in the education space'
+                        ]
+                    }
+                ]
+            },
+            {
+                year: '2011',
+                minor: [
+                    {
+                        month: 'August 2011',
+                        events: [
+                            'UI developer on Android app - FusionFace'
+                        ]
+                    },
+                    {
+                        month: 'April 2011',
+                        events: [
+                            'Worked on proof-of-concept Android app as part of Multunus labs'
+                        ]
+                    },
+                    {
+                        month: 'March 2011',
+                        events: [
+                            'Completed 2 successful years'
+                        ]
+                    },
+                    {
+                        month: 'Jan 2011',
+                        events: [
+                            'Worked on fourth product - ElixirHealth.com - web app in the Consumer Health Space, with e-commerce, content management and search as key pieces'
+                        ]
+                    }
+                ]
+            },
+            {
+                year: '2010',
+                minor: [
+                    {
+                        month: 'September 2010',
+                        events: [
+                            'Worked on third product - Publink.com - web app with Location Based Search service'
+                        ]
+                    },
+                    {
+                        month: 'March 2010',
+                        events: [
+                            'Finished 1 year at Multunus'
+                        ]
+                    },
+                    {
+                        month: 'Feb 2010',
+                        events: [
+                            'Joined Multunus hiring team. Started interviewing candidates.'
+                        ]
+                    },
+                    {
+                        month: 'Jan 2010',
+                        events: [
+                            'Awarded "Star of Multunus" for the year 2009'
+                        ]
+                    }
+                ]
+            },
+            {
+                year: '2009',
+                minor: [
+                    {
+                        month: 'September 2009',
+                        events: [
+                            'Worked on second product - PurpleLetter.org - an online letter sending service'
+                        ]
+                    },
+                    {
+                        month: 'May 2009',
+                        events: [
+                            'Worked on first product - a web app with Face Recognition technology'
+                        ]
+                    },
+                    {
+                        month: 'March 2009',
+                        events: [
+                            'Joined Multunus'
+                        ]
+                    }
+                ]
+            }
+
+        ]
+    }, 
+    {
+        name: 'tamil', 
+        display_name: 'Tamil',
+        roles: '',
+        major: [
+            {
+                year: '',
+                minor: [
+                    {
+                        month: '',
+                        event: ''
+                    }
+                ]
+            }
+        ]
+    }, 
+    {
+        name: 'akshay', 
+        display_name: 'Akshay'
+    }, 
+    {
+        name: 'sreenath', 
+        display_name: 'Sreenath'
+    }, 
+    {
+        name: 'anup', 
+        display_name: 'Anup'
+    }, 
+    {
+        name: 'ernest', 
+        display_name: 'Ernest'
+    }, 
+    {
+        name: 'abhilash', 
+        display_name: 'Abhilash'
+    }, 
+    {
+        name: 'kp', 
+        display_name: 'KP'
+    }, 
+    {
+        name: 'you', 
+        display_name: 'You?'
+    }
+];
 
 function renderTurkTemplate(){
-    var template = _.template($('#turk-template').html());
-    _.each(Turks, function(turk){
-        $('.the-turks').append(template({name: turk.name, display_name: turk.display_name, popover_for_turk: 'manoj'}));
+    var turkTemplate = _.template($('#turk-template').html());
+    var timelineMinorTemplate = _.template($('#timelineMinor-template').html());
+    var timelineMajorTemplate = _.template($('#timelineMajor-template').html());
+    var timelineContainerTemplate = _.template($('#timelineContainer-template').html());
+    _.each(Timelines, function(turk){
+        $('.the-turks').append(turkTemplate({name: turk.name, display_name: turk.display_name, popover_for_turk: 'manoj'}));
+        turk.majorTemplate = timelineMajorTemplate;
+        turk.minorTemplate = timelineMinorTemplate;
+        $('.timelines').append(timelineContainerTemplate(turk));
     });
 }
